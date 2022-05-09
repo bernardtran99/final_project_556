14e575346a365fd73e6f9fca17fa35f35ef80efb
bernardtran99 <bernardtran99@gmail.com>
2022-05-09 15:07:02 -0400

    update all

--- sensor_web_server/map.py
+++ sensor_web_server/map.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('table.html')
+    return render_template('content.html')
 
 if __name__ == "__main__":
     app.run("0.0.0.0")
--- sensor_web_server/process.py
+++ sensor_web_server/process.py
@@ -5,39 +5,14 @@ import os
 import re
 from datetime import datetime
 from flask import *
-
-network_list = {}
-display_table = {}
-
-# def get_table():
-#     now = datetime.now()
-#     current_time = now.strftime("%H:%M:%S")
-#     network_list["TIME"] = current_time
-
-#     result = subprocess.Popen(["sudo","iwlist","wlan0","scan"],stdout=subprocess.PIPE, universal_newlines=True)
-#     output, error = result.communicate()
-
-#     for line in output.split("\n"):
-
-#         if re.search('Signal',line):
-#             signal = re.search('Signal level=(.*) dBm',line)
-#             signal_level = []
-#             signal_level = signal.group(1)
-        
-#         if re.search('ESSID',line):
-#             essid = re.search('ESSID:"(.*)"',line)
-#             network_list[essid.group(1)] = signal_level
-#             # print(essid.group(1))
-
-#     return network_list
+import json
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    # display_table = get_table()
-    # return render_template('table.html', display_table=display_table)
-    return render_template('table.html')
+    return render_template('content.html')
 
 if __name__ == "__main__":
     app.run("0.0.0.0")

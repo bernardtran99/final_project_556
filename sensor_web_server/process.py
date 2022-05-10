e0b6d15151453d7406b77d80d31c044a5a806640
bernardtran99 <bernardtran99@gmail.com>
2022-05-10 07:07:16 -0400

    update al

--- sensor_web_server/process.py
+++ sensor_web_server/process.py
@@ -40,15 +40,27 @@ def get_state():
     global is_open
     return is_open
 
+def get_unlock():
+    global last_unlock
+    return last_unlock
+
+def unlock_door():
+    global last_unlock
+    print("Unlocking Door")
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    last_unlock = current_time
+
 @app.route("/", methods = ['GET', 'POST'])
 def index():
     if request.method == 'POST':
         if request.form.get('unlock') == 'TRUE':
-            print("Unlocking Door")
+            unlock_door()
     current_ring = get_last_ring()
     current_prox = get_last_prox()
     current_state = get_state()
-    return render_template('content.html', current_ring = current_ring, current_prox = current_prox, current_state = current_state)
+    current_lock = get_unlock()
+    return render_template('content.html', current_ring = current_ring, current_prox = current_prox, current_state = current_state, current_lock = current_lock)
 
 if __name__ == "__main__":
     t_main = threading.Thread(target = main_thread)

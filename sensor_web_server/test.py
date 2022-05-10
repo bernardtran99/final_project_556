dfc6595d0ff9df4de3a335450fd8f586a5b33b5b
bernardtran99 <bernardtran99@gmail.com>
2022-05-10 01:24:45 -0400

    Update test.py

--- sensor_web_server/test.py
+++ sensor_web_server/test.py
@@ -1,13 +1,18 @@
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+import time
+
+x = 0
 
 def button_callback(channel):
-    print("Button was pushed!")
+    time.sleep(0.01)
+    x += 1
+    print(x)
 
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(29, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
-GPIO.add_event_detect(29,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
+GPIO.add_event_detect(29,GPIO.RISING,callback=button_callback,bouncetime=300) # Setup event on pin 10 rising edge
 
 message = input("Press enter to quit\n\n") # Run until someone presses enter
 

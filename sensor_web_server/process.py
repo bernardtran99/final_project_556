b0c7c56723a40bb5a5976cd8b06adc311f389674
bernardtran99 <bernardtran99@gmail.com>
2022-05-10 05:17:45 -0400

    Update process.py

--- sensor_web_server/process.py
+++ sensor_web_server/process.py
@@ -1,5 +1,6 @@
 import sys
 import time
+import threading
 import subprocess
 import os
 import re
@@ -11,35 +12,11 @@ from flask_socketio import SocketIO
 last_ring = 0
 last_prox = 0
 is_open = False
+app = Flask(__name__)
 
-# def get_table():
-#     now = datetime.now()
-#     current_time = now.strftime("%H:%M:%S")
-#     network_list["TIME"] = current_time
-
-#     result = subprocess.Popen(["sudo","iwlist","wlan0","scan"],stdout=subprocess.PIPE, universal_newlines=True)
-#     output, error = result.communicate()
-
-#     for line in output.split("\n"):
-
-#         if re.search('Signal',line):
-#             signal = re.search('Signal level=(.*) dBm',line)
-#             signal_level = []
-#             signal_level = signal.group(1)
-        
-#         if re.search('ESSID',line):
-#             essid = re.search('ESSID:"(.*)"',line)
-#             network_list[essid.group(1)] = signal_level
-#             # print(essid.group(1))
-
-#     return network_list
-
-# app = Flask(__name__)
-
-# @app.route("/")
-# def index():
-#     display_table = get_table()
-#     return render_template('table.html', display_table=display_table)
+def main_thread():
+    global app
+    app.run("0.0.0.0")
 
 def get_last_ring():
     global last_ring
@@ -53,8 +30,6 @@ def get_state():
     global is_open
     return is_open
 
-app = Flask(__name__)
-
 @app.route("/")
 def index():
     current_ring = get_last_ring()
@@ -63,4 +38,5 @@ def index():
     return render_template('content.html', current_ring = current_ring, current_prox = current_prox, current_state = current_state)
 
 if __name__ == "__main__":
-    app.run("0.0.0.0")
+    t1 = threading.Thread(target = main_thread)
+    t1.start()
